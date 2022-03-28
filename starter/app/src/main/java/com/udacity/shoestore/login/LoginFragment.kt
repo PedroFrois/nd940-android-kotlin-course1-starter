195795eea6b85f0e7dfa62542a0a3d66fd0e5677
Pedro Frois Bittencourt <p.f.bit@hotmail.com>
2022-03-28 09:19:51 -0300

    fix: navigation fix

--- starter/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -29,7 +30,7 @@ class LoginFragment : Fragment() {
 
         viewModel.eventNavigateWelcome.observe(viewLifecycleOwner) { newEvent ->
             if (newEvent) {
-                // TODO:  findNavController().navigate()
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
                 viewModel.onEventNavigateToWelcomeFinish()
             }
         }
