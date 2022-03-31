b050c1ffa734bbc6364b5586455fd47c9b28be55
Pedro Frois Bittencourt <p.f.bit@hotmail.com>
2022-03-31 20:51:36 -0300

    feat: listing new shoes

--- starter/app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.databinding.ItemShoeListBinding
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListFragment : Fragment() {
 
@@ -23,7 +25,7 @@ class ShoeListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentShoeListBinding.inflate(inflater, container, false)
-        _viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+        _viewModel = ViewModelProvider(requireActivity()).get(ShoeListViewModel::class.java)
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -37,7 +39,32 @@ class ShoeListFragment : Fragment() {
             }
         }
 
+        viewModel.shoes.observe(viewLifecycleOwner) { newListOfShoes ->
+            newListOfShoes.forEach {
+                binding.shoesListLinearLayout.addView(
+                    createShoeListItemView(
+                        inflater,
+                        container,
+                        it
+                    ),
+                    binding.shoesListLinearLayout.childCount - 1
+                )
+            }
+        }
+
         return binding.root
+    }
+
+    private fun createShoeListItemView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        shoe: Shoe
+    ): View {
+        return ItemShoeListBinding.inflate(inflater, container, false).apply {
+            fieldName.text = shoe.name
+            fieldCompany.text = shoe.company
+            fieldSize.text = shoe.size.toString()
+        }.root
     }
 
     override fun onDestroy() {
