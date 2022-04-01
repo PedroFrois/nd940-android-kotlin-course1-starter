4d4356e7e12f36f3255bc0f81f487304537b46d2
Pedro Frois Bittencourt <p.f.bit@hotmail.com>
2022-04-01 08:07:41 -0300

    fix: navigation code review

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
@@ -10,19 +13,36 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private var _navController: NavController? = null
+    private val navController: NavController get() = _navController!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        _navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(
             this,
-            findNavController(R.id.myNavHostFragment)
+            navController
         )
         Timber.plant(Timber.DebugTree())
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.logout_item -> {
+            navController.navigate(NavGraphDirections.actionGlobalToLoginFragment())
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.myNavHostFragment).navigateUp()
+        return navController.navigateUp()
     }
 }
